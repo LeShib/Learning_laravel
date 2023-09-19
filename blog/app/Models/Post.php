d3f21a26c14ac8f77bba11d4d00e67298957d8ac
Nikko <dibernardonikko@gmail.com>
2023-09-19 12:14:47 +0200

    lesson 11

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -8,9 +8,10 @@ class Post
     public static function all(){
         $files = File::files(resource_path("posts/"));
 
-        return array_map(function($file){
-            return 'foo';
-        }, $files);
+        // return array_map(function($file){
+        //     return $file->getContents();
+        // }, $files);
+        return array_map(fn($file) => $file->getContents(), $files);
     }
 
     public static function find($slug){
