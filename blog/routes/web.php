e1bfc18c9991f65946d1585cce82fac0c0266ec7
Nikko <dibernardonikko@gmail.com>
2023-09-21 10:36:48 +0200

    lesson 11.5

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 
@@ -15,8 +16,17 @@ use App\Models\Post;
 */
 
 Route::get('/', function () {
+    $files = File::files(resource_path("posts/"));
+
+    $posts = collect($files)->map(function ($file){
+        $document = YamlFrontMatter::parseFile($file);
+
+        return new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
+    });
+
+    // ddd($posts);
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => $posts
     ]);
 });
 
