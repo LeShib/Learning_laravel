be6799cef14e167fb133af7759c859218928b35b
Nikko <dibernardonikko@gmail.com>
2023-09-21 14:06:08 +0200

    lesson 12

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -6,12 +6,15 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    /* VARIABLES */
     public $title;
     public $excerpt;
     public $date;
     public $body;
     public $slug;
 
+    /* FUNCTIONS */
+    // Constructor
     public function __construct($title, $excerpt, $date, $body, $slug){
         $this->title = $title;
         $this->excerpt = $excerpt;
@@ -20,6 +23,7 @@ class Post
         $this->slug = $slug;
     }
 
+    // Get all posts
     public static function all(){
         return collect(File::files(resource_path("posts")))->map(fn ($file) => YamlFrontMatter::parseFile($file))->map(fn($document) => new Post(
             $document->title, 
@@ -30,17 +34,8 @@ class Post
         ));
     }
 
+    // find a post with its slug
     public static function find($slug){
-        // base_path();
-        // if(!file_exists($path = resource_path("posts/{$slug}.html"))){
-        //     // abort(404);
-        //     throw new ModelNotFoundException();
-        // }
-
-        // return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
-        // of all the blog posts, find the one with a slug that matches the one that was requested.
-
         $posts = static::all();
 
         return $posts->firstWhere('slug', $slug);
