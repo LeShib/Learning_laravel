b54025547ca29d74feb96682a5aee6f30ba8451f
Nikko <dibernardonikko@gmail.com>
2023-09-21 14:02:44 +0200

    lesson 12

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -2,6 +2,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
@@ -20,21 +21,28 @@ class Post
     }
 
     public static function all(){
-        $files = File::files(resource_path("posts/"));
-
-        // return array_map(function($file){
-        //     return $file->getContents();
-        // }, $files);
-        return array_map(fn($file) => $file->getContents(), $files);
+        return collect(File::files(resource_path("posts")))->map(fn ($file) => YamlFrontMatter::parseFile($file))->map(fn($document) => new Post(
+            $document->title, 
+            $document->excerpt, 
+            $document->date, 
+            $document->body(), 
+            $document->slug
+        ));
     }
 
     public static function find($slug){
-        base_path();
-        if(!file_exists($path = resource_path("posts/{$slug}.html"))){
-            // abort(404);
-            throw new ModelNotFoundException();
-        }
+        // base_path();
+        // if(!file_exists($path = resource_path("posts/{$slug}.html"))){
+        //     // abort(404);
+        //     throw new ModelNotFoundException();
+        // }
 
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        // return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+
+        // of all the blog posts, find the one with a slug that matches the one that was requested.
+
+        $posts = static::all();
+
+        return $posts->firstWhere('slug', $slug);
     }
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -16,17 +16,8 @@ use App\Models\Post;
 */
 
 Route::get('/', function () {
-    $files = File::files(resource_path("posts/"));
-
-    $posts = collect($files)->map(function ($file){
-        $document = YamlFrontMatter::parseFile($file);
-
-        return new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
-    });
-
-    // ddd($posts);
     return view('posts', [
-        'posts' => $posts
+        'posts' => Post::all()
     ]);
 });
 
